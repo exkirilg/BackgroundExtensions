052ce4c2d025953b24f44461d70eb864aa0929e5
Кирилл <kirilg.ex@gmail.com>
2023-05-04 22:53:45 +0400

    feat: store manager

--- BackgroundExtensions/BackgroundServices/StoreManager.cs
+++ BackgroundExtensions/BackgroundServices/StoreManager.cs
@@ -1,4 +1,5 @@
-﻿using BackgroundExtensions.Models;
+﻿using BackgroundExtensions.DbAccess;
+using BackgroundExtensions.Models;
 using BackgroundExtensions.Services;
 
 namespace BackgroundExtensions.BackgroundServices;
@@ -6,12 +7,16 @@ namespace BackgroundExtensions.BackgroundServices;
 public class StoreManager : BackgroundService
 {
     private readonly string _storeDirectory;
+    private readonly string _deprecated;
     private readonly IExtensionValidator _extensionValidator;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public StoreManager(IConfiguration configuration, IExtensionValidator extensionValidator)
+    public StoreManager(IConfiguration configuration, IExtensionValidator extensionValidator, IServiceScopeFactory scopeFactory)
     {
         _storeDirectory = configuration.GetSection("fs:store").Value!;
+        _deprecated = configuration.GetSection("fs:deprecated").Value!;
         _extensionValidator = extensionValidator;
+        _scopeFactory = scopeFactory;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -20,11 +25,21 @@ public class StoreManager : BackgroundService
         {
             foreach (var file in Directory.GetFiles(_storeDirectory))
             {
-                Extension extension;
-                if (_extensionValidator.TryValidateExtensionFile(file, out extension) == false)
-                    continue;
-                
+                FileInfo fi = new(file);
 
+                if (_extensionValidator.TryValidateExtensionFile(file, out Extension extension) == false
+                    || File.Exists(extension.FileName))
+                {
+                    File.Move(fi.FullName, Path.Combine(fi.Directory!.FullName, $"{_deprecated}{fi.Name}"));
+                }
+
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    var dbAccess = scope.ServiceProvider.GetRequiredService<IDbAccess>();
+                    await dbAccess.PostExtension(extension);
+                }
+
+                File.Move(fi.FullName, extension.FileName);
             }
 
             await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
